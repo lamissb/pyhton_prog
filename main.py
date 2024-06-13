f015a4a859b6eb09213635823ef59457dbafe8c8
lamissb <107209872+lamissb@users.noreply.github.com>
2024-06-13 15:13:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ liste1= [8, 9, 3, 6, 1, 10]
 liste1.reverse("the reversed list is " , liste1)
 
 liste2=[91, 67, 120, 34, 76, 54, 78, 87, 56, 64, 345]
-lsite2.sort()
-print("the sorted list is" , list2)
+liste2.sort()
+print("the sorted list is" , liste2)
 
 liste3 = []
 liste3 = liste1.copy()
